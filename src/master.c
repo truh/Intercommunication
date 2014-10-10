8a526fbe9a589668d299d44eb00cfb2442eb11b7
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-10-10 16:48:13 +0200

    moved void InitConsole(void) to util.c

--- src/master.c
+++ src/master.c
@@ -10,28 +10,6 @@
 
 #include "util.h"
 
-void InitConsole(void)
-{
-	// Enable GPIO Port A, used for UART0 pins.
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-    // Configure the pin muxing for UART0 functions on port A0 and A1.
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-
-    // Enable UART0 so that we can configure the clock.
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-
-    // Use the internal 16MHz oscillator as the UART clock source.
-    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-
-    // Select the alternate (UART) function for these pins.
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    // Initialize the UART for console I/O.
-    UARTStdioConfig(0, 115200, 16000000);
-}
-
 void OnDataReceived(void);
 
 int main(void)
