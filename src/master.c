b2f6a7b27e67bb4ac7f6145829c0b6bb5a55bb2c
Andreas Willinger <awi95@gmx.at>
2014-10-10 16:28:34 +0200

    Added interrupt handlers to the master

--- src/master.c
+++ src/master.c
@@ -8,28 +8,6 @@
 #include "driverlib/uart.h"
 #include "utils/uartstdio.h"
 
-//! This example uses the following peripherals and I/O signals.  You must
-//! review these and change as needed for your own board:
-//! - SSI0 peripheral
-//! - GPIO Port A peripheral (for SSI0 pins)
-//! - SSI0Clk - PA2
-//! - SSI0Fss - PA3
-//! - SSI0Rx  - PA4
-//! - SSI0Tx  - PA5
-//!
-//! The following UART signals are configured only for displaying console
-//! messages for this example.  These are not required for operation of I2C0.
-//! - UART0 peripheral
-//! - GPIO Port A peripheral (for UART0 pins)
-//! - UART0RX - PA0
-//! - UART0TX - PA1
-
-//*****************************************************************************
-//
-// This function sets up UART0 to be used for a console to display information
-// as the example is running.
-//
-//*****************************************************************************
 void InitConsole(void)
 {
 	// Enable GPIO Port A, used for UART0 pins.
@@ -52,6 +30,8 @@ void InitConsole(void)
     UARTStdioConfig(0, 115200, 16000000);
 }
 
+void OnDataReceived(void);
+
 int main(void)
 {
     // Set the clocking to run directly from the external crystal/oscillator.
@@ -61,11 +41,6 @@ int main(void)
     // Set up the serial console to use for displaying messages.  This is
     // just for this example program and is not needed for SSI operation.
     InitConsole();
-
-    // Display the setup on the console.
-    UARTprintf("SSI ->\n");
-    UARTprintf("  Mode: TI\n");
-    UARTprintf("  Data: 8-bit\n\n");
 
     // The SSI0 peripheral must be enabled for use.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
@@ -103,45 +78,48 @@ int main(void)
     // The "non-blocking" function checks if there is any data in the receive
     // FIFO and does not "hang" if there isn't.
     while(SSIDataGetNonBlocking(SSI0_BASE, NULL));
-
-
-    // Display indication that the SSI is transmitting data.
-    UARTprintf("Sent:\n  ");
-
-    // Send 3 bytes of data.
-    for(ui32Index = 0; ui32Index < NUM_SSI_DATA; ui32Index++)
-    {
-        // Display the data that SSI is transferring.
-        UARTprintf("'%c' ", pui32DataTx[ui32Index]);
-
-        // Send the data using the "blocking" put function.  This function
-        // will wait until there is room in the send FIFO before returning.
-        // This allows you to assure that all the data you send makes it into
-        // the send FIFO.
-        SSIDataPut(SSI0_BASE, pui32DataTx[ui32Index]);
-    }
+	
+	/*
+	SSIDataPut(SSI0_BASE, pui32DataTx[ui32Index]);
 
     // Wait until SSI0 is done transferring all the data in the transmit FIFO.
-    while(SSIBusy(SSI0_BASE))
-    {
-    }
+    while(SSIBusy(SSI0_BASE));
 
-    // Display indication that the SSI is receiving data.
-    UARTprintf("\nReceived:\n  ");
+	SSIDataGet(SSI0_BASE, &pui32DataRx[ui32Index]);
 
-    // Receive 3 bytes of data.
-    for(ui32Index = 0; ui32Index < NUM_SSI_DATA; ui32Index++)
-    {
-        // Receive the data using the "blocking" Get function. This function
-        // will wait until there is data in the receive FIFO before returning.
-        SSIDataGet(SSI0_BASE, &pui32DataRx[ui32Index]);
+	// Since we are using 8-bit data, mask off the MSB.
+	pui32DataRx[ui32Index] &= 0x00FF;*/
+	
+	// OnDataReceived will be the interrupt
+	IntRegister(INT_SSI0, OnDataReceived);
 
-        // Since we are using 8-bit data, mask off the MSB.
-        pui32DataRx[ui32Index] &= 0x00FF;
-
-        // Display the data that SSI0 received.
-        UARTprintf("'%c' ", pui32DataRx[ui32Index]);
-    }
-
+	// Enable SPI interrupt	
+	IntEnable(INT_SSI0);
+	
+	// SPI interrupt from receiving data
+	// TODO Set when to call the interrupt
+	SSIIntEnable(SSI0_BASE, SSI_RXFF);
+	
+	// Interrupt enable
+	IntMasterEnable();
+	
     return(0);
+}
+
+void OnDataReceived(void)
+{
+	UARTprintf("\n\n*** DATA RECEIVED ***");
+	
+	unsigned long int_source = SSIIntStatus(SSI0_BASE, true);
+	unsigned long rx_data_size;
+	
+	SSIIntClear(SSI0_BASE, int_source);
+	
+	if(int_source & SSI_RXTO)
+	{
+		bool received = false;
+		rx_data_size = SSIDataGetNonBlocking(SSI0_BASE, &received);
+		
+		UARTprintf("\nData received via SSI: %s", (received)?"true":"false");
+	}
 }
