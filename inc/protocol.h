511e8495934bdf68cdc5397680a85fa8557b9a85
Andreas Willinger <awi95@gmx.at>
2014-10-15 13:28:03 +0200

    Preparing

--- inc/protocol.h
+++ inc/protocol.h
@@ -1,12 +1,9 @@
 #ifndef _PROTOCOL_HEADER_
 #define _PROTOCOL_HEADER_
 
-// Data size, in bits
-#define PAYLOAD_SIZE 10
-
-//  #ID ??? I
-// |---|---|-|
-// 
-// Soon to come
+// LED's
+#define P_LED_RED (1<<0)
+#define P_LED_GREEN (1<<1)
+#define P_LED_BLUE (1<<2)
 
 #endif
